f881c214fc83555ec7a88fe153dc7e3ef6b38cb4
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2026-09-11 19:57:55 +0200

    Updated the .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,10 +28,7 @@ module.exports = {
           ['@plone/volto', `${coreLocation}/packages/volto/src`],
           ['@plone/volto-slate', `${coreLocation}/packages/volto-slate/src`],
           ['@plone/registry', `${coreLocation}/packages/registry/src`],
-          [
-            'volto-aemet',
-            './packages/volto-aemet/src',
-          ],
+          ['volto-aemet', './packages/volto-aemet/src'],
           ...addonAliases,
         ],
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
